4f5fe6edde0004a10b77fd19df5c971b67a9766e
Jason Walsh <j@wal.sh>
2015-07-22 19:48:27 -0400

    Communication Model provides the UI for HOA

--- core.js
+++ core.js
@@ -1,62 +1,19 @@
+var embed = document.getElementById('embed');
+embed.contentWindow.postMessage('core.js', '*');
+
 var showParticipants = function() {
-    var participants = gapi.hangout.getParticipants();
-    var retVal = '<h2>Participants:</h2><ul>';
     console.log(participants);
-    for (var index in participants) {
-        var participant = participants[index];
-
-        if (!participant.person) {
-            retVal += '<li>A participant not running this app</li>';
-        }
-
-        var broadcaster = '';
-        if (participant.isBroadcaster) {
-            broadcaster = '<b>*</b>';
-        }
-        retVal += '<li>' +
-            broadcaster +
-            '<img width="16" height="16" src="' +
-            participant.person.image.url +
-            '"/> ' +
-            participant.person.displayName +
-            ' <tt>' + participant.person.id + '</tt>' +
-            '</li>';
-    }
-    retVal += '</ul>';
-    var div = document.getElementById('participants');
-    div.innerHTML = retVal;
+    embed.contentWindow.postMessage(JSON.stringify(participants), '*');
 };
 
-var showParticipantsPolling = setInterval(showParticipants, 2000);
-
-var showCollaboration = function() {
-    var participants = gapi.hangout.getParticipants();
-    var retVal = 'None';
-    var time = (new Date()).getTime();
-    console.log(time, 'showCollaboration()', participants);
-    if (participants) {
-        for (var index in participants) {
-            var participant = participants[index];
-
-            if (participant && participant.person &&
-                participant.isBroadcaster) {
-                console.log('Broadcasting:',
-                            participant.person.displayName);
-                retVal = '<tt>' + time + ',' +
-                    participant.person.id  + ',' +
-                    participant.person.displayName + '</tt>';
-            }
-        }
-    }
-    var div = document.getElementById('collaboration');
-    div.innerHTML = '<h2>Broadcaster:</h2>' + retVal;
-};
+var showParticipantsPolling = setInterval(showParticipants, 5000);
 
 var talkers = {};
 
 // TODO: Throttle
 gapi.hangout.av.onVolumesChanged.add(
     function(evt) {
+        embed.contentWindow.postMessage(JSON.stringify(evt), '*');
         var recorderId = gapi.hangout.getLocalParticipantId();
         var volumes = evt.volumes;
         var user = 'unknown';
@@ -64,7 +21,6 @@ gapi.hangout.av.onVolumesChanged.add(
         // merge volumes into known talker state
         for (var k in volumes) {
             var talker = gapi.hangout.getParticipantById(k);
-            console.log('Talking', talker);
             var i = new Image();
             i.src = 'https://wal.sh/collaboration-scoring/_.gif?' +
                 (new Date()).getTime() + ',' +
@@ -79,7 +35,4 @@ gapi.hangout.av.onVolumesChanged.add(
             }
         }
 
-        var div = document.getElementById('talking');
-        div.innerHTML = '<h2>Talking:</h2><pre>' +
-            JSON.stringify(talkers) + '</pre>';
     });
